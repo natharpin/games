44ce3a44237cbb0fe9d155d02b3106cdb7511308
Nathan Andre Arpin <narpin@mscs.mu.edu>
2017-03-27 20:45:54 -0500

    Changed where we get images

--- src/dropper/entities/Coin.java
+++ src/dropper/entities/Coin.java
@@ -1,48 +1,39 @@
 package dropper.entities;
 
-import javafx.animation.Animation;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.animation.Interpolator;
-import javafx.animation.Transition;
 
 public class Coin extends Sprite {
-	
-	private int i = 0;
-	private Image[] imageArray = new Image[2];
-	
-	public Coin(double x, double y, double size){
-		this.x = x;
-		this.y = y;
-		this.width = size;
-		this.height = size;
-		imageArray[0] = new Image("pics/front.png");
-		imageArray[1] = new Image("pics/flip.png");		
-	}
-	
-	public void update() {
-		i=(i+1)%20;
-	}
 
-	public void render(GraphicsContext gc) {
-    	gc.drawImage(imageArray[i/10], x, y);
-	}
+    private int i = 0;
+    private static Image[] imageArray = new Image[]{
+        new Image("pics/front.png"), new Image("pics/flip.png")
+    };
 
-	public void flipCoin(){
-		
-	}
-	
-	public boolean intersects(Ball b){
-		double cx = x + width / 2;
-		double cy = y + height / 2;
-		double bx = b.x + b.width / 2;
-		double by = b.y + b.width / 2;
-		
-		double distance = Math.sqrt(Math.pow(bx - cx, 2) + Math.pow(by - cy, 2));
-		if(distance < (width / 2) + (b.width / 2))
-			return true;
-		return false;
-	}
-	
+    public Coin(double x, double y, double size) {
+        this.x = x;
+        this.y = y;
+        this.width = size;
+        this.height = size;
+    }
+
+    public void update() {
+        i = (i + 1) % 20;
+    }
+
+    public void render(GraphicsContext gc) {
+        gc.drawImage(imageArray[i / 10], x, y);
+    }
+
+    public boolean intersects(Ball b) {
+        double cx = x + width / 2;
+        double cy = y + height / 2;
+        double bx = b.x + b.width / 2;
+        double by = b.y + b.width / 2;
+
+        double distance = Math.sqrt(Math.pow(bx - cx, 2) + Math.pow(by - cy, 2));
+        if (distance < (width / 2) + (b.width / 2)) return true;
+        return false;
+    }
+
 }
